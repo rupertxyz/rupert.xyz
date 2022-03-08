9b6b35806cb699562e34193982fb5bc1336c9185
supertrupert <rupert.hoffschmidt@gmail.com>
2022-03-08 21:57:37 +0100

    build nav

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,27 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faMoon } from '@fortawesome/free-regular-svg-icons'
+import { titleFlexContainer, nav } from './header.module.css';
+import { Link } from 'gatsby';
 
 const Header = ({data}) => {
 
   return (
       <header>
-          <h1>{data.site.siteMetadata.author}</h1>
-          <ul>
-              <a href="https://twitter.com/rupertxyz"><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></a>
-              <a href="#"><FontAwesomeIcon icon={faMoon}></FontAwesomeIcon></a>
-          </ul>
+          <div className={titleFlexContainer}>
+            <h1>{data.site.siteMetadata.author}</h1>
+            <ul>
+                <a href="https://twitter.com/rupertxyz"><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></a>
+                <a href="#"><FontAwesomeIcon icon={faMoon}></FontAwesomeIcon></a>
+            </ul>
+          </div>
+          <nav className={nav}>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/">Articles</Link></li>
+              <li><Link to="/airtable">Airtable</Link></li>
+            </ul>
+          </nav>
       </header>
   )
 }
---
+++ src/pages/airtable.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+import Wrapper from '../components/wrapper'
+
+const Airtable = () => {
+  return (
+    <Wrapper>
+    </Wrapper>
+  )
+}
+
+export default Airtable
