78d5dfee8a3b3a929a2d0463dc0a84341f63e892
supertrupert <rupert.hoffschmidt@gmail.com>
2022-03-09 12:21:24 +0100

    new commit

--- src/components/header.js
+++ src/components/header.js
@@ -2,18 +2,26 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faMoon } from '@fortawesome/free-regular-svg-icons'
-import { titleFlexContainer, nav } from './header.module.css';
+import { titleFlexContainer, nav, titleFlexContainerLeft } from './header.module.css';
 import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const Header = ({data}) => {
+
+  function clickHandler () {
+    console.log("hello")
+  }
 
   return (
       <header>
           <div className={titleFlexContainer}>
-            <h1>{data.site.siteMetadata.author}</h1>
+            <div className={titleFlexContainerLeft}>
+              <Link to="/"><StaticImage width={50} src="../images/kopf.gif"></StaticImage></Link>
+              <h1>{data.site.siteMetadata.author}</h1>
+            </div>
             <ul>
                 <Link to="https://twitter.com/rupertxyz"><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></Link>
-                <Link><FontAwesomeIcon icon={faMoon}></FontAwesomeIcon></Link>
+                <Link style={{cursor: "pointer"}}><FontAwesomeIcon onClick={clickHandler} icon={faMoon}></FontAwesomeIcon></Link>
             </ul>
           </div>
           <nav className={nav}>
