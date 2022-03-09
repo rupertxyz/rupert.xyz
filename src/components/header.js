4dc8e1782dcae43fb4730fdde985013263e32178
supertrupert <rupert.hoffschmidt@gmail.com>
2022-03-09 12:37:04 +0100

    another commit

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faMoon } from '@fortawesome/free-regular-svg-icons'
 import { titleFlexContainer, nav, titleFlexContainerLeft } from './header.module.css';
 import { Link } from 'gatsby';
-import { StaticImage } from 'gatsby-plugin-image';
+import kopfGif from '../images/kopf.gif'
 
 const Header = ({data}) => {
 
@@ -12,7 +12,7 @@ const Header = ({data}) => {
       <header>
           <div className={titleFlexContainer}>
             <div className={titleFlexContainerLeft}>
-              <Link to="/"><StaticImage width={50} src="../images/kopf.gif"></StaticImage></Link>
+              <Link to="/"><img width={50} src={kopfGif}></img></Link>
               <h1>{data.site.siteMetadata.author}</h1>
             </div>
             <ul>
