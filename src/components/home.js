2f592cee6fa194d8e20b435961d273e12847b333
Rupert Hoffschmidtm <rupert.hoffschmidt@gmail.com>
2022-08-11 12:45:23 +0200

    update home page

--- src/components/home.js
+++ src/components/home.js
@@ -10,7 +10,7 @@ const Home = () => {
         <StaticImage className={heroImage} src="../images/sunrise.jpg" alt="sunrise"></StaticImage>
         <h1>Me in 10 Seconds</h1>
         <ul style={{fontSize: "1.3rem", lineHeight:"2rem"}}>
-          <li>I like falling down rabbit holes and try to write <Link to="/articles">articles</Link> about it.</li>
+          <li>I like falling down rabbit holes and write <Link to="/articles">articles</Link> about it.</li>
           <li>I'm an entrepreneur who founded two companies in completely unrelated fields: <Link to="https://hy.co/">hy!</Link> and <Link to="https://roykombucha.com/">ROY</Link>.</li>
           <li>I'm an Airtable (and aspiring software) developer, <Link to="/airtable">so please reach out to me if you need help.</Link></li>
           <li>Based in Berlin, Germany.</li>
