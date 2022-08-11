ea2e14e3fc65b5cecf56c158a1fdaf0f207cff7e
Rupert Hoffschmidtm <rupert.hoffschmidt@gmail.com>
2022-08-11 12:26:50 +0200

    test

--- src/pages/airtable.js
+++ src/pages/airtable.js
@@ -1,9 +1,19 @@
 import * as React from "react"
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import { Link } from "gatsby"
 
 const Airtable = () => {
   return (
     <Layout pageTitle="Airtable">
+      <div style={{display: "flex", gap: "25px", alignItems: "center", marginBottom: "5rem"}} className="airtable-hero">
+        <h1><Link to="https://airtable.com">Airtable.</Link></h1>
+        <StaticImage src="../images/sample_image.jpg"></StaticImage>
+      </div>
+      <div style={{display: "flex", gap: "25px", alignItems: "center"}}>
+        <StaticImage src="../images/airtable_clinic_image.jpg"></StaticImage>
+        <h1>I help companies build world-class internal tools with #lowcode and #nocode software, with a focus on <Link to="https://airtable.com">Airtable</Link></h1>
+      </div>
     </Layout>
   )
 }
