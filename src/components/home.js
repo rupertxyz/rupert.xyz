c45b558a84f72b270cb75422a1f17b7644df140d
Rupert Hoffschmidtm <rupert.hoffschmidt@gmail.com>
2022-08-20 09:10:21 +0200

    update

--- src/components/home.js
+++ src/components/home.js
@@ -1,22 +1,38 @@
-import React from 'react'
-import Layout from './layout.js'
-import { StaticImage } from 'gatsby-plugin-image'
-import { heroImage } from './home.module.css'
-import { Link } from 'gatsby'
+import React from 'react';
+import Layout from './layout.js';
+import { StaticImage } from 'gatsby-plugin-image';
+import { heroImage } from './home.module.css';
+import { Link } from 'gatsby';
 
 const Home = () => {
   return (
     <Layout>
-        <StaticImage className={heroImage} src="../images/sunrise.jpg" alt="sunrise"></StaticImage>
-        <h1>Me in 10 Seconds</h1>
-        <ul style={{fontSize: "1.3rem", lineHeight:"2rem"}}>
-          <li>I like falling down rabbit holes and write <Link to="/articles">articles</Link> about it.</li>
-          <li>I'm an entrepreneur who founded two companies in completely unrelated fields: <Link to="https://hy.co/">hy!</Link> and <Link to="https://roykombucha.com/">ROY</Link>.</li>
-          <li>I'm a no- &amp; low-code developer (mostly Airtable), <a href="https://twitter.com/rupertxyz">so please send me a dm if you need help.</a></li>
-          <li>Based in Berlin, Germany.</li>
-        </ul>
+      <StaticImage
+        className={heroImage}
+        src="../images/sunrise.jpg"
+        alt="sunrise"
+      ></StaticImage>
+      <h1>Me in 10 Seconds</h1>
+      <ul style={{ fontSize: '1.3rem', lineHeight: '2rem' }}>
+        <li>
+          Entrepreneur who founded two companies in completely unrelated fields:{' '}
+          <Link to="https://hy.co/">hy!</Link> and{' '}
+          <Link to="https://roykombucha.com/">ROY</Link>.
+        </li>
+        <li>
+          No- &amp; low-code developer (mostly Airtable).{' '}
+          <a href="https://twitter.com/rupertxyz">
+            Please send a dm if you need help.
+          </a>
+        </li>
+        <li>
+          Random thoughts on life, work and things in between:{' '}
+          <Link to="/articles">Articles</Link>.
+        </li>
+        <li>Based in Berlin, Germany.</li>
+      </ul>
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
